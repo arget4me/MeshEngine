4dbde38b1b7f681d6942e8078c3ae6f10b39315f
arget4me <sn.jn@telia.com>
2022-01-01 12:07:29 +0100

    Add io file types

---
+++ src/io/types_io.h
@@ -0,0 +1,18 @@
+#ifndef IO_TYPES_HEADER
+#define IO_TYPES_HEADER
+
+#include <common.h>
+
+namespace MESHAPI
+{
+
+struct FullFile
+{
+    uint8* buffer;
+    uint32 buffer_size;
+};
+
+
+}
+
+#endif
